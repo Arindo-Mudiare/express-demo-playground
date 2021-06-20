652b728586a2fd03f443c3b4be737cf0ad1722d9
Irapfaya <arindomudiare@gmail.com>
2021-06-20 11:29:51 +0100

    created routes folder for better routing

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const friendsRouter = require("./routes/friends.router");
+const messagesRouter = require("./routes/messages.router");
+
+const app = express();
+
+const PORT = 3000;
+
+app.use(express.json());
+
+app.use("/friends", friendsRouter);
+app.use("/messages", messagesRouter);
+
+app.listen(PORT, () => {
+  console.log(`app listening at http://localhost:${PORT}`);
+});
